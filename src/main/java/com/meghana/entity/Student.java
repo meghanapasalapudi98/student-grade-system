a47ebd77728006a7d3b73dd75f48dd06add51187
Meghana Pasalapudi <meghanapasalapudi98@gmail.com>
2026-06-11 14:43:41 -0500

    Add automatic grade calculation logic

--- src/main/java/com/meghana/entity/Student.java
+++ src/main/java/com/meghana/entity/Student.java
@@ -1,53 +1,68 @@
-    package com.meghana.entity;
+package com.meghana.entity;
 
-    import jakarta.persistence.Entity;
-    import jakarta.persistence.GeneratedValue;
-    import jakarta.persistence.GenerationType;
-    import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-    @Entity
-    public class Student {
+@Entity
+public class Student {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        private String name;
-        private int marks;
-        private String grade;
+    private String name;
+    private int marks;
+    private String grade;
 
-        public Student() {
-        }
+    public Student() {
+    }
 
-        public Long getId() {
-            return id;
-        }
+    public Long getId() {
+        return id;
+    }
 
-        public void setId(Long id) {
-            this.id = id;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        public String getName() {
-            return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public int getMarks() {
-            return marks;
-        }
+    public int getMarks() {
+        return marks;
+    }
 
-        public void setMarks(int marks) {
-            this.marks = marks;
-        }
+    public void setMarks(int marks) {
+        this.marks = marks;
+        this.grade = calculateGrade(marks);
+    }
 
-        public String getGrade() {
-            return grade;
-        }
-
-        public void setGrade(String grade) {
-            this.grade = grade;
+    private String calculateGrade(int marks) {
+        if (marks >= 90) {
+            return "A";
+        } else if (marks >= 80) {
+            return "B";
+        } else if (marks >= 70) {
+            return "C";
+        } else if (marks >= 60) {
+            return "D";
+        } else {
+            return "F";
         }
     }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
